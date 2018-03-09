4fcac59376dc8bfcf932a1409a961e43b7b95d56
Joris Palings <joris@palings.be>
2018-03-09 14:06:48 +0100

    Removed express

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
+const ws = require('ws');
+const turf = require('@turf/turf');
+
 const config = {
-  port: process.env.PORT || 6969,
+  port: 6969,
   maxFOV: 45, // degrees
   maxDistance: 1000 // km
 }
 
-const ws = require('ws');
-const turf = require('@turf/turf');
-const express = require('express');
-const webSocketServer = require('ws').Server;
-
-const app = express()
-  .listen(config.port, () => {
-    console.info(`Listening on port ${config.port}`);
-  });
-
-const server = new webSocketServer({ server: app });
+const server = new ws.Server({
+  port: process.env.PORT || config.port
+});
 
 const clients = [
   { id: 'Noorden',  latitude: 51.859528,  longitude: 4.645805 },
